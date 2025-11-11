24de34886abe3c6992839b69ab05e82a731b1337
Jesse-home-linux <jesse.islam@gmail.com>
2025-11-11 11:21:32 -0500

    base template using marp

---
+++ marp_config.js
@@ -0,0 +1,12 @@
+// marp.config.js
+module.exports = {
+  // tell Marp where your custom theme(s) live
+  themeSet: [
+    './themes/micm_theme.css'
+  ],
+  // default options for every run
+  options: {
+    theme: 'micm_theme',
+    allowLocalFiles: true
+  }
+};
